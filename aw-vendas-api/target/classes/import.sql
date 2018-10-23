0efa96813446fc0292dbd0df7037a4d62541049a
Felipe Bezerra de Assis <felipe.bezerra@db1.com.br>
2018-10-23 09:45:16 -0300

    Finalizada a API

--- aw-vendas-api/target/classes/import.sql
+++ aw-vendas-api/target/classes/import.sql
@@ -1,7 +1,7 @@
 insert into clientedto (id,nome) values (1,'Eletrônicos Eletro');
 insert into clientedto (id,nome) values (2,'Smart Compras');
 
-insert into produtodto (id,nome,valor) values(1,'PC',1000.00)
+insert into produtodto (id,nome,valor) values(1,'Notebook',3900.00)
 insert into produtodto (id,nome,valor) values (2,'Mouse',100.00);
 
 insert into vendadto (id, clientedto_id, frete, total, cadastro) values (1,1,15.0,2965.0,sysdate());
